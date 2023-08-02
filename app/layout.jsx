c2a5559fd3a14ef15f860e22f655b16ddccf8039
Phil Snow <phil.snow@prismic.io>
2023-08-02 18:27:49 +0200

    delete routes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
 import { Inter, Space_Grotesk } from 'next/font/google'
 import '@/styles/tailwind.css'
 import Header from '@/components/Header'
-import { PrismicPreview } from '@prismicio/next'
-import { repositoryName } from '@/prismicio'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,7 +20,6 @@ export default function RootLayout({ children }) {
         <div className={`${inter.variable} ${space_grotesk.variable}`}>
           <Header />
           {children}
-          <PrismicPreview repositoryName={repositoryName} />
         </div>
       </body>
     </html>
